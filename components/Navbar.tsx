a17a48a037d62d448de3b270e1818d807c8c0be3
behnamhamidi1991 <eslp91@hotmail.com>
2023-12-30 00:27:46 +0330

    Navbar done, navigation functionality done.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <div className="fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20">
       <div className="flex flex-row gap-3 items-center">
-        <div>
+        <div className="relative">
           <Image
             src="/horseLogo.jpg"
             alt="logo"
