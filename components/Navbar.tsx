741a1701699480466d1aeaf76676718d8d2316cd
behnamhamidi1991 <eslp91@hotmail.com>
2023-12-30 00:09:47 +0330

    Navbar second part

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,15 @@ function Navbar() {
   return (
     <div className="fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20">
       <div className="flex flex-row gap-3 items-center">
-        <Image src="/horseLogo.jpg" alt="logo" width={38} height={38} />
+        <div>
+          <Image
+            src="/horseLogo.jpg"
+            alt="logo"
+            width={38}
+            height={38}
+            className="w-full h-full object-contain rounded-full"
+          />
+        </div>
 
         <h1 className="text-white text-[25px] font-semibold">BenDev</h1>
       </div>
